0806d006b496c6fd2fe26bb3ed1e87967566b0ed
Silas Joisten <silas.joisten@exozet.com>
2017-03-22 15:42:18 +0100

    added userpropvider for sonata

--- User/UserProvider.php
+++ User/UserProvider.php
@@ -4,6 +4,8 @@ namespace Exozet\Oauth2LoginBundle\User;
 
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
+use Sonata\UserBundle\Entity\UserManager;
+use Sonata\UserBundle\Model\UserManagerInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -11,21 +13,25 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 class UserProvider implements OAuthAwareUserProviderInterface, UserProviderInterface
 {
     /**
-     * {@inheritDoc}
+     * @var UserManager
+     */
+    private $userManager;
+
+    /**
+     *
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @inheritdoc()
      */
     public function loadUserByUsername($username)
     {
-        return new User(
-            $username,
-            null,
-            array(),
-            true,
-            null,
-            null,
-            true,
-            true,
-            true
-        );
+        return $this->userManager->findUserByUsernameOrEmail($username);
     }
 
     /**
@@ -33,14 +39,22 @@ class UserProvider implements OAuthAwareUserProviderInterface, UserProviderInter
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
-        return new User(
-            $response->getEmail(),
-            $response->getTokenSecret(),
-            ['ROLE_SONATA_ADMIN'],
-            true,
-            $response->getProfilePicture(),
-            $response->getRealName()
-        );
+        $user = $this->loadUserByUsername($response->getEmail());
+
+        if(!$user) {
+            $user = $this->userManager->create();
+            $user->setUsername($response->getUsername());
+            $user->setEmail($response->getEmail());
+            $user->setFirstname($response->getFirstName());
+            $user->setLastname($response->getLastName());
+            $user->setPassword('');
+            $user->setEnabled(true);
+            $user->setRoles(['ROLE_SONATA_ADMIN']);
+
+            $this->userManager->save($user);
+        }
+
+        return $user;
     }
 
     /**
@@ -52,17 +66,7 @@ class UserProvider implements OAuthAwareUserProviderInterface, UserProviderInter
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
         }
 
-        return new User(
-            $user->getUsername(),
-            $user->getPassword(),
-            $user->getRoles(),
-            $user->isEnabled(),
-            $user->getProfilePicture(),
-            $user->getFullName(),
-            $user->isAccountNonExpired(),
-            $user->isCredentialsNonExpired(),
-            $user->isAccountNonLocked()
-        );
+        return $this->loadUserByUsername($user->getUsername());
     }
 
     /**
